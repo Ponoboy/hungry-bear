edb7f4f357807dd65196a653710ff99ae4725c94
Ponoboy <Kazuboi30@gmail.com>
2017-06-20 14:58:02 -1000

    Add files via upload

---
+++ hungry-bear/app.js
@@ -0,0 +1,7 @@
+function eatHumans(bearName, humanNames){
+	console.log(bearName);
+	console.log(humanNames);
+	console.log("Jethro ate Kazu, Kai, " + humanNames[2])
+}
+
+eatHumans("Jethro", ["Kazu", "Kai", "Jayce"])
